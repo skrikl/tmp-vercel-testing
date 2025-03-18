3248d57958b4e778dfa277bc2dbd86458eeb6885
Sergey Kriklivyy <sergey.kriklivyy@gmail.com>
2025-03-18 20:20:50 -0300

    vercel db integration testing prep

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+            <h1>Customers page</h1>
+            <p>Is alive</p>
+        </>
+    );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="border-2 border-gray-200 p-4">
+            <p>Invoices Page</p>
+        </div>
+    );
+}
